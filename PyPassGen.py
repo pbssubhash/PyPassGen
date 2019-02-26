5e09e22dcfd719b5d8b5f221b9cb2844f3cf9b69
Nicolás Hasbún A <nhasbun@gmail.com>
2019-02-26 15:09:58 -0300

    Pretty print + copy to clipboard + push button to exit
    Main idea is to keep the file as a standalone executable for quick execution when creating new passwords.

--- PyPassGen.py
+++ PyPassGen.py
@@ -4,6 +4,7 @@
 #This will generate Random strong passwords for you ! :)
 
 import random
+import pyperclip
 
 symbol = 0
 lower = 0
@@ -12,8 +13,8 @@ number = 0
 count = 0
 password = []
 
-length = input("Hey, Welcome. Just say me how many characters do you want in your password ?")
-length = int(length)
+length = input("Hey, Welcome. Just say me how many characters do you want in your password? (default 128)\n")
+length = 128 if length is '' else int(length)
 
 #randomly select ascii character classes and individual characters
 
@@ -48,6 +49,15 @@ while count < length:
 #convert ascii code to characters
 word = "".join([chr(c) for c in password])
 
+#copy pass to clipboard
+pyperclip.copy(word)
+
 #print the result
-print ("Random",length,"character password =      ",word,"")
-print ("It contains",lower,"lowercase,",upper,"uppercase,",number,"numbers and",symbol,"symbol characters")
+print ("Random password of length %s is: \n" % length)
+
+print('******')
+print(word)
+print('******')
+
+print ("\nIt contains",lower,"lowercase,",upper,"uppercase,",number,"numbers and",symbol,"symbol characters")
+input('Password copied to clipboard, push a button to exit...')
