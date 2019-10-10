710b26802f7bc3a9f2046014090329625d56dba4
Nicolás Hasbún A <nhasbun@gmail.com>
2019-10-10 16:23:33 -0300

    Fix range correction for random characters generation based on ascii tables

--- PyPassGen.py
+++ PyPassGen.py
@@ -19,27 +19,32 @@ length = 128 if length is '' else int(length)
 #randomly select ascii character classes and individual characters
 
 while count < length:
-    rand = random.randint (0,3)
+    # randint is lower and higher inclusive
+    rand = random.randint (0,2)
     if rand == 0:
+        # a-z
         lower += 1
-        b = int(random.randint (97,123))
+        b = random.randint (97,122)
         password.append(b)
     elif rand == 1:
+        # A-Z
         upper += 1
-        b = random.randint (65,91)
+        b = random.randint (65,90)
         password.append(b)
     elif rand == 2:
+        # 0-9
         number += 1
-        b = random.randint (48,58)
+        b = random.randint (48,57)
         password.append(b)
     elif rand == 3:
+        # special characters
         r = random.randint(0,2)
         symbol += 1
         if r == 0:
-            b = random.randint (33,48)
+            b = random.randint (33,47)
             password.append(b)
         elif r == 1:
-            b = random.randint (91,97)
+            b = random.randint (91,96)
             password.append(b)
         elif r == 2:
             b = random.randint (123,126)
